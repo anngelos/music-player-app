a66c3a4383e444c43d9ecbc04958b55c3b043c72
Ângelo Cardoso <hoanngelos@gmail.com>
2023-03-07 15:14:50 -0300

    applying unit tests.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,10 @@
 <script></script>
 
 <template>
-  <h1>Hi, I'm <b>ABOUT</b>!</h1>
+  <div class="about">
+    <h1>Hi, I'm about!</h1>
+    <!-- <base-button />-->
+  </div>
 </template>
 
 <style></style>
